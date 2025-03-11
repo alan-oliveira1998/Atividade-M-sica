21c7724dbe51941175a6b923def8d73b0af7d03e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-11 20:43:19 -0300

    Resolvi a Atividade

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.ForegroundColor=ConsoleColor.Red;
+Console.Write(@"I Wanna Be Yours");
+Console.WriteLine();
+Console.WriteLine();
+Console.ResetColor();
+Console.ForegroundColor=ConsoleColor.Blue;
+Console.Write(@"I wanna be your vacuum cleaner
+Breathing in your dust
+I wanna be your Ford Cortina
+I will never rust");
+Console.WriteLine();
+Console.ResetColor();
+Console.ForegroundColor=ConsoleColor.Blue;
+Console.Write(@"If you like your coffee hot
+Let me be your coffee pot
+You call the shots, babe
+I just wanna be yours");
+Console.WriteLine();
+Console.ResetColor();
+Console.ForegroundColor=ConsoleColor.Blue;
+Console.Write(@"Secrets I have held in my heart
+Are harder to hide than I thought
+Maybe I just wanna be yours
+I wanna be yours, I wanna be yours");
+Console.WriteLine();
+Console.ResetColor();
+Console.ForegroundColor=ConsoleColor.Yellow;
+Console.Write(@"Wanna be yours
+Wanna be yours
+Wanna be yours");
+Console.ResetColor();
